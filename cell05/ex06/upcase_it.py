06609b8ee1d279e2492cb9da98accac0933e8a42
kchuayno <korrawitden@gmail.com>
2025-06-19 03:38:40 +0000

    5.6

---
+++ cell05/ex06/upcase_it.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("none")
+    else:
+        text = " ".join(sys.argv[1:])
+        print(text.upper())
+
+
+if  __name__ == "__main__":
+    main()
